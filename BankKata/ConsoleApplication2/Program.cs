867bdb4198f54e8b0f974401ebe4d1fce0a90114
Joe Hobbs <Joe Hobbs@DESKTOP-5FG7PMG>
2016-10-20 11:33:21 +0100

    Changed namespaces of solutions

--- BankKata/ConsoleApplication2/Program.cs
+++ BankKata/ConsoleApplication2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.IO;
 
-namespace ConsoleApplication1
+namespace BankKata
 {
     class Program
     {
--- BankKata/ConsoleApplication2/Properties/AssemblyInfo.cs
+++ BankKata/ConsoleApplication2/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("BankKata")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("BankKata")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("439ed2b6-fd1b-448b-a7a0-171d82406377")]
+[assembly: Guid("9001d19c-a2da-4a71-bc95-32a718c2e0a8")]
 
 // Version information for an assembly consists of the following four values:
 //
